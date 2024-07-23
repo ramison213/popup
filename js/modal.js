e62b0900efba49c0b75c5e738ad80ed5bd26e30a
pavlo <ramison213@gmail.com>
2024-07-23 17:26:02 +0300

    changed styles, refactoring

--- js/modal.js
+++ js/modal.js
@@ -1,45 +1,45 @@
-document.querySelectorAll('.show-modal') //получаем вск кнопки show modal
+document.querySelectorAll('.show-modal') // get all show modal buttons
     .forEach($btn => {
-        $btn.addEventListener('click', () => { // для каждой кнопки по клику
-            let $modal = document.querySelector('#' + $btn.dataset.modalName); // получаем id элемента
-            console.log('открытие модалки');
-            
+        $btn.addEventListener('click', () => {
+            let $modal = document.querySelector('#' + $btn.dataset.modalName); // get the element by id
+            console.log('opening modal');
+
             $modal.hidden = false;
 
-            document.onkeydown = (e) => { // вешаем событие на esc				
-                if (e.keyCode === 27) {
-                    console.log('событие на esc');                    
-                    $modal.hidden = true;             
+            document.onkeydown = (e) => { // event on esc key
+                if (e.key === 'Escape') {
+                    console.log('esc event');
+                    $modal.hidden = true;
                 }
 
-                document.onkeydown = null; // после отработки события удаляем обработчик события
+                document.onkeydown = null; // remove event handler after it triggers
             }
         })
     })
 
-document.querySelectorAll('.modal-overlay') // для каждого оверлея
+document.querySelectorAll('.modal-overlay') // get all modal overlays
     .forEach($overlay => {
-        $overlay.addEventListener('click', () => { // событие по клику
+        $overlay.addEventListener('click', () => {
             $overlay.hidden = true;
-            console.log('событие: клик по оверлею');            
+            console.log('event: click on overlay');
             document.onkeydown = null;
         })
 
         let $modal = $overlay.querySelector('.modal-content');
 
-        $modal.addEventListener('click', event => { // получаем элемент модальное окно
-            console.log('убираем всплытие');            
-            event.stopPropagation(); // убираем всплытие событий
+        $modal.addEventListener('click', event => {
+            console.log('prevent bubbling');
+            event.stopPropagation();
         })
 
-        let $closeBtns = $overlay.querySelectorAll('.modal-close'); // получаем все кнопки закрыть окно
+        let $closeBtns = $overlay.querySelectorAll('.modal-close'); // get all close buttons
 
         $closeBtns.forEach($closeBtn => {
-            $closeBtn.addEventListener('click', () => { //для каждой кнопки закрыть окно событие по клику
-                console.log('клик по кнопке close');
-                
+            $closeBtn.addEventListener('click', () => {
+                console.log('click on close button');
+
                 $overlay.hidden = true;
                 document.onkeydown = null;
             })
         })
-    })	
+    })
